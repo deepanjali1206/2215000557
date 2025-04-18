157adb764566e6e17d8563e606534a4ca9a283c9
deepanjali1206 <deepanjalisingh0612@gmail.com>
2025-04-18 11:44:32 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./Components/Navbar";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <div>
+      <Navbar></Navbar>
+    </div>
+  );
+}
+
+export default App;
